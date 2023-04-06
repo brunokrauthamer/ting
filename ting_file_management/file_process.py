34927b212057a7251796f9d12a673db9fbc20155
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-04-06 16:54:19 -0300

    req03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    length = instance.__len__()
+    for i in range(length):
+        data = instance.search(i)
+        if data["nome_do_arquivo"] == path_file:
+            return None
+    file_list = txt_importer(path_file)
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_list),
+        "linhas_do_arquivo": file_list
+    }
+    instance.enqueue(file_info)
+    sys.stdout.write(str(file_info))
 
 
 def remove(instance):
