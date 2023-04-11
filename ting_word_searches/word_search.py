adfa8a000b61318fef5afbd87351ea70fa983bf6
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-04-11 18:41:50 -0300

    req07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue_len = instance.__len__()
+    occurances_info = []
+    word_lower = word.lower()
+    for i in range(queue_len):
+        info = instance.search(i)
+        lines = info["linhas_do_arquivo"]
+        ocorrencias = []
+        for index, line in enumerate(lines):
+            line_lower = line.lower()
+            if word_lower in line_lower:
+                ocorrencias.append({ "linha": index + 1 })
+        if bool(ocorrencias):
+            dict_info = {
+                "palavra": word,
+                "arquivo": info["nome_do_arquivo"],
+                "ocorrencias": ocorrencias
+            }
+            occurances_info.append(dict_info)
+    return occurances_info
 
 
 def search_by_word(word, instance):
